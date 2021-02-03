9a29560537ec2a73698d6bf6d3aa96d8a1153072
Ron Mendoza <ron@gisd88.com>
2021-02-03 19:16:20 +0800

    Fist Commit

---
+++ ConsoleLauncher/Program.cs
@@ -0,0 +1,29 @@
+﻿using GISD.Automation.Framework.Core;
+using PlaywrightSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace ConsoleLauncher
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var playwright = await Playwright.CreateAsync();
+            
+            await using var browser = await playwright.Chromium.LaunchAsync(headless: false);
+           
+            var context = await browser.NewContextAsync();
+            var page = await context.NewPageAsync();
+            
+            // Log and continue all network requests
+            await page.RouteAsync("**", (route, _) =>
+            {
+                Console.WriteLine(route.Request.Url);
+                route.ContinueAsync();
+            });
+
+            await page.GoToAsync("http://todomvc.com");
+        }
+    }
+}
---
+++ GISD.Automation.Framework.Core/AutomationCore.cs
@@ -0,0 +1,22 @@
+﻿
+using PlaywrightSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace GISD.Automation.Framework.Core
+{
+    public class AutomationCore
+    {
+        public async Task<bool> Run()
+        {
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GoToAsync("http://www.bing.com");
+            
+            //await page.ScreenshotAsync(path: outputFile);
+
+            return await Task.FromResult(true);
+        }
+    }
+}
